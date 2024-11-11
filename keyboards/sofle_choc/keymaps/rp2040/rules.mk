c42a0ed9132d3b5ba59ab9e71452b66ad47fc78b
Kevin Panaro <panaro.kevin@gmail.com>
2024-11-11 10:05:14 -0800

    initial choc commit

---
+++ keyboards/sofle_choc/keymaps/rp2040/rules.mk
@@ -0,0 +1,10 @@
+# RP2040 microcontroller
+CONVERT_TO=rp2040_ce
+
+ENCODER_MAP_ENABLE = yes
+
+# Tri Layers
+TRI_LAYER_ENABLE = yes
+
+# layer switching when command + fn key is pressed
+KEY_OVERRIDE_ENABLE = yes
