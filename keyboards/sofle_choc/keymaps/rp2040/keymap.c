f7eaef2d21ab14f6e5591e86155ac0e60acf88c1
kevinpanaro <panaro.kevin@gmail.com>
2024-11-18 17:47:29 -0800

    testing out different keymaps for dev

--- keyboards/sofle_choc/keymaps/rp2040/keymap.c
+++ keyboards/sofle_choc/keymaps/rp2040/keymap.c
@@ -13,7 +13,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include "action.h"
 #include "action_layer.h"
+#include "action_util.h"
+#include "keycodes.h"
 #include "oled_driver.h"
 #include QMK_KEYBOARD_H
 
@@ -25,8 +28,15 @@ enum layer_names {
     UPPER,
     ADJUST,
 #endif
+    COMMAND,
     TOTAL_LAYERS,
 };
+
+#if defined(COMBO_ENABLE)
+enum combos {
+    CMD_LAYER,
+};
+#endif // defined(COMBO_ENABLE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
@@ -46,11 +56,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 
 [MACOS] = LAYOUT(
-    KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_GRV,
+    KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS,
     KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,   KC_MPLY,KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                      TL_UPPR, KC_LCTL, KC_LOPT, KC_LCMD, KC_SPC,    KC_ENT, KC_RCMD,  KC_RCMD,  KC_RGUI, TL_LOWR
+    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,   KC_F20,KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                      TL_UPPR, KC_LCTL, KC_LOPT, KC_LCMD, KC_SPC,            KC_ENT, TL_LOWR, KC_RCMD,  KC_ROPT,  KC_RCTL
 ),
 /*
  * TRI_LAYER_LOWER_LAYER
@@ -70,10 +80,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #if defined(TRI_LAYER_ENABLE)
 [LOWER] = LAYOUT(
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, _______
+    KC_GRV , KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,    KC_F12,
+    _______, KC_EQL,  KC_MINS, KC_LPRN, KC_RPRN, KC_PLUS,                         KC_COLN, KC_LBRC, KC_RBRC, KC_UNDS, KC_PERC, KC_PIPE,
+    _______, KC_EXLM, KC_AT ,  KC_HASH, KC_LCBR, KC_RCBR, _______,       _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_DLR,  KC_BSLS, _______,
+                      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, _______,  XXXXXXX,  XXXXXXX, XXXXXXX
 ),
 /*
  * TRI_LAYER_UPPER_LAYER
@@ -94,14 +104,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [UPPER] = LAYOUT(
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     KC_LEFT, KC_UP  , KC_DOWN, KC_RIGHT,XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, _______
+                      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, _______,  XXXXXXX,  XXXXXXX, XXXXXXX
 ),
 /*
  * TRI_LAYER_ADJUST_LAYER
  * ╭──────┬──────┬──────┬──────┬──────┬──────╮ ╭───╮       ╭───╮  ╭──────┬──────┬──────┬──────┬──────┬──────╮
- * │ MAKE │   1  │   2  │   3  │   4  │   5  │ │   │       │   │  │   6  │   7  │   8  │   9  │   0  │  -   │
+ * │ BOOT │   1  │   2  │   3  │   4  │   5  │ │   │       │   │  │   6  │   7  │   8  │   9  │   0  │  -   │
  * ├──────┼──────┼──────┼──────┼──────┼──────┤ │   │       │   │  ├──────┼──────┼──────┼──────┼──────┼──────┤
  * │ ESC  │   Q  │   W  │   E  │   R  │   T  │ │   │       │   │  │   Y  │   U  │   I  │   O  │   P  │ Bspc │
  * ├──────┼──────┼──────┼──────┼──────┼──────┤ ╰───╯       ╰───╯  ├──────┼──────┼──────┼──────┼──────┼──────┤
@@ -115,30 +125,138 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 
 [ADJUST] = LAYOUT(
-    QK_MAKE, XXXXXXX, XXXXXXX, XXXXXXX , XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX , XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX , XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, RGB_RMOD, RGB_TOG, RGB_MOD,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, _______
+                      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, _______,  XXXXXXX,  XXXXXXX, XXXXXXX
 ),
 #endif // defined(TRI_LAYER_ENABLE)
+
+/*
+ * TRI_LAYER_ADJUST_LAYER
+ * ╭──────┬──────┬──────┬──────┬──────┬──────╮ ╭───╮       ╭───╮  ╭──────┬──────┬──────┬──────┬──────┬──────╮
+ * │      │   1  │   2  │   3  │   4  │   5  │ │   │       │   │  │   6  │   7  │   8  │   9  │   0  │  -   │
+ * ├──────┼──────┼──────┼──────┼──────┼──────┤ │   │       │   │  ├──────┼──────┼──────┼──────┼──────┼──────┤
+ * │ ESC  │   Q  │   W  │   E  │   R  │   T  │ │   │       │   │  │   Y  │   U  │   I  │   O  │   P  │ Bspc │
+ * ├──────┼──────┼──────┼──────┼──────┼──────┤ ╰───╯       ╰───╯  ├──────┼──────┼──────┼──────┼──────┼──────┤
+ * │ Tab  │   A  │   S  │RGB_RM│RGB_T │RGB_M ├───────╮    ╭───────┤   H  │   J  │   K  │   L  │   ;  │  '   │
+ * ├──────┼──────┼──────┼──────┼──────┼──────┤   ◯   │    │   ◯   ├──────┼──────┼──────┼──────┼──────┼──────┤
+ * │LShift│   Z  │   X  │   C  │   V  │   B  ├───────┤    ├───────┤   N  │   M  │   ,  │   .  │   /  │RShift│
+ * ╰──────┴──────┼──────┼──────┼──────┼──────┤       │    │       ├──────┼──────┼──────┼──────┼──────┴──────╯
+ *               │UPPER │ LCTL │ LOPT │ LCMD │ Space │    │ Enter │ RCMD │ ROPT │ RALT │LOWER │
+ *               ╰──────┴──────┴──────┴──────┤       │    │       ├──────┴──────┴──────┴──────╯
+ *                                           ╰───────╯    ╰───────╯
+*/
+#if defined(KEY_OVERRIDE_ENABLE) || defined(COMBO_ENABLE)
+[COMMAND] = LAYOUT(
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX , XXXXXXX, XXXXXXX,                     KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX, XXXXXXX,
+    XXXXXXX, KC_EQL,  KC_MINS, KC_LPRN , KC_RPRN, KC_PLUS,                     KC_COLN, KC_LBRC, KC_RBRC, KC_UNDS, KC_PERC, KC_PIPE,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                              _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, _______,  XXXXXXX,  XXXXXXX, XXXXXXX
+),
+#endif  // defined(KEY_OVERRIDE_ENABLE)
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    return true;
 };
 
 #if defined(KEY_OVERRIDE_ENABLE)
+// This doesn't work because left and right command are the same key
+bool momentary_layer(bool key_down, void *layer) {
+    if (key_down) {
+        layer_on((uint8_t)(uintptr_t)layer);
+    } else {
+        layer_off((uint8_t)(uintptr_t)layer);
+    }
 
-#endif
+    return false;
+}
+const key_override_t cmd_override = {.trigger_mods          = MOD_BIT(KC_LCMD) | MOD_BIT(KC_RCMD),                       //
+                                   .layers                 = ~0,                                          //
+                                   .suppressed_mods        = MOD_BIT(KC_LCMD) | MOD_BIT(KC_RCMD),                       //
+                                   .options                = ko_option_activation_trigger_down,                 //
+                                   .negative_mod_mask      = (uint8_t) ~(MOD_BIT(KC_LCMD) | MOD_BIT(KC_RCMD)),          //
+                                   .custom_action          = momentary_layer,                                           //
+                                   .context                = (void *)COMMAND,                                          //
+                                   .trigger                = KC_NO,                                                     //
+                                   .replacement            = KC_NO,                                                     //
+                                   .enabled                = NULL};
+const key_override_t *key_overrides[] = {
+    &cmd_override,
+};
+#endif  // defined(KEY_OVERRIDE_ENABLE)
+
+#if defined(COMBO_ENABLE)
+const uint16_t PROGMEM cmd_combo[] = {KC_LCMD, KC_RCMD, COMBO_END};
+
+combo_t key_combos[] = {
+    [CMD_LAYER] = COMBO(cmd_combo, MO(COMMAND))
+};
+#endif // defined(COMBO_ENABLE)
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [MACOS] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
+    [MACOS] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_F17, KC_F11) },
 #if defined(TRI_LAYER_ENABLE)
     [LOWER] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
     [UPPER] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
     [ADJUST] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
 #endif  // defined(TRI_LAYER_ENABLE)
+
+#if defined(KEY_OVERRIDE_ENABLE) || defined(COMBO_ENABLE)
+    [COMMAND] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
+#endif  // defined(KEY_OVERRIDE_ENABLE) || defined(COMBO_ENABLE)
 };
 #endif  // defined(ENCODER_MAP_ENABLE)
 
+#if defined(ENCODER_ENABLE)
+uint8_t mod_state;
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    mod_state = get_mods();
+    clear_mods();
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    } else if (index == 1) { /* Second encoder */
+        if (mod_state & MOD_MASK_GUI) { /* Command */
+
+            if (clockwise) {
+                tap_code(KC_F16);  /* VSCode Debug : Continue */
+            } else {
+                // unregister_mods(KC_LGUI);
+                // register_code(KC_LSFT);
+                tap_code(KC_F11);  /* VSCode Debug : Step Out */
+                // unregister_code(KC_LSFT);
+                // register_mods(KC_LGUI);
+            }
+        } else if (mod_state & MOD_MASK_ALT) { /* Option */
+            register_code(KC_LOPT);
+            if (clockwise) {
+                tap_code(KC_RIGHT);  /* Next Word*/
+                // unregister_code(KC_LOPT);
+            } else {
+                // clear_mods();
+                tap_code(KC_LEFT);  /* Previous Word */
+            }
+            unregister_code(KC_LOPT);
+        } else {
+            if (clockwise) {
+                tap_code(KC_F17);  /* VSCode Debug : Step Over */
+            } else {
+                tap_code(KC_F11);  /* VSCode Debug : Step Into */
+            }
+        }
+    }
+    set_mods(mod_state);
+    return false;
+}
+#endif // defined(ENCODER_ENABLE)
 
 #if defined(OS_DETECTION_ENABLE)
 bool process_detected_host_os_user(os_variant_t detected_os) {
@@ -161,10 +279,12 @@ bool process_detected_host_os_user(os_variant_t detected_os) {
 
 // Rotate the slave OLED
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    if (!is_keyboard_master()) {
-        return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+    if (is_keyboard_master()) {
+        return OLED_ROTATION_270;
     }
-
+    else {
+        return OLED_ROTATION_0;
+    }
     return rotation;
 }
 
@@ -173,6 +293,18 @@ void oled_current_layer(void) {
     switch (get_highest_layer(layer_state)) {
         case MACOS:
             oled_write_P(PSTR("MacOS"), false);
+            break;
+        case LOWER:
+            oled_write_P(PSTR("Lower"), false);
+            break;
+        case UPPER:
+            oled_write_P(PSTR("Raise"), false);
+            break;
+        case ADJUST:
+            oled_write_P(PSTR("Adjust"), false);
+            break;
+        case COMMAND:
+            oled_write_P(PSTR("cmd"), false);
             break;
         default:
             oled_write_ln_P(PSTR("Undef"), false);
--- keyboards/sofle_choc/keymaps/rp2040/rules.mk
+++ keyboards/sofle_choc/keymaps/rp2040/rules.mk
@@ -8,7 +8,11 @@ TRI_LAYER_ENABLE = yes
 
 # Key Overrides : https://docs.qmk.fm/features/key_overrides
 # layer switching when command + fn key is pressed
-# KEY_OVERRIDE_ENABLE = yes
+KEY_OVERRIDE_ENABLE = no
+
+# Combos : https://docs.qmk.fm/features/combo
+# monentary layer switch when held
+COMBO_ENABLE = no
 
 # OS Detection : https://docs.qmk.fm/features/os_detection
 OS_DETECTION_ENABLE = yes
@@ -22,3 +26,8 @@ WPM_ENABLE = no
 # RGB Matrix : https://docs.qmk.fm/features/rgb_matrix
 # Custom : https://docs.qmk.fm/features/rgb_matrix#custom-rgb-matrix-effects
 RGB_MATRIX_CUSTOM_USER = yes
+
+# Encoders : https://docs.qmk.fm/features/encoders
+# Better encoders to support modifiers
+ENCODER_MAP_ENABLE = no
+ENCODER_ENABLE = yes
